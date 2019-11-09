dcc3b9c784c07d9931cd2d20460d395bebd81c99
ypablo <yfedora@gmail.com>
2019-11-09 17:05:32 +0000

    Add styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage"
 import { Switch, Route } from "react-router-dom"
+import Shop from "./pages/shop/shop"
 
 function App() {
 
   return (
     <div>
       <Switch>
-        <Route path="/" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={Shop} />
       </Switch>
     </div>
   );
---
+++ src/components/preview/preview.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./preview.styles.scss"
+import CollectionItem from "../collection-item/collection-item"
+
+const PreviewCollection = ({ title, items }) => (
+    <div className="collection-preview">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <div className="preview">
+            {items.filter((item, index) => index < 4).map(({ id, ...otherItemProps }) => (
+                <CollectionItem key={id} {...otherItemProps} />
+            ))}
+        </div>
+    </div>
+)
+
+export default PreviewCollection
