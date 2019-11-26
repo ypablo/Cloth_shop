892cf1b432edbb2fe850d506cd8383e01ee07f89
ypablo <yfedora@gmail.com>
2019-11-26 19:25:39 +0000

    Add cart icon value accumulator

--- src/components/cart-icon/cart-icon.comp.jsx
+++ src/components/cart-icon/cart-icon.comp.jsx
@@ -4,10 +4,10 @@ import "./cart-icon.styles.scss"
 import {connect} from "react-redux" 
 import {toggleCartHidden} from "../../redux/cart/cart.actions"
 
-const CartIcon =({toggleCartHidden}) => (
+const CartIcon =({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
@@ -15,4 +15,9 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = ({cart:{cartItems}}) => ({
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
