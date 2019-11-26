33bd03ef78a884578bd7b8b716bb32b6d9873bad
ypablo <yfedora@gmail.com>
2019-11-26 17:58:16 +0000

    Created cart-item comp and connected our cart dropdown with cartitems reducer

--- src/components/cart-dropdown/cart.comp.jsx
+++ src/components/cart-dropdown/cart.comp.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import CustomButton from "../custom-button/button"
 import "./cart.styles.scss"
+import CartItem from "../cart-item/cart-item.comp"
+import {connect} from "react-redux"
 
-export default function Cart() {
-    return (
+const CartDropdown = ({cartItems}) => (
         <div className="cart-dropdown">
-            <div className="cart-items"/>
+            <div className="cart-items">
+                {
+                    cartItems.map(cartItem => (
+                        <CartItem key={cartItem.id} item={cartItem}/>
+                     ))}
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
-    )
-}
+)
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)
---
+++ src/components/cart-item/cart-item.comp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./cart-item.styles.scss"
+
+export default function cartItem({item: {imageUrl, price, name, quantity}}) {
+    return (
+        <div className="cart-item">
+            <img src={imageUrl} alt="item"/>
+            <div className="item-details">
+                <span className="name">{name}</span>
+                <span className="price">{quantity} x ${price} = ${quantity * price}</span>
+            </div>
+        </div>
+    )
+}
